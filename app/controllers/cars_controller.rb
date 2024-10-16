3a506b6a33d3bf8bb0958531029d984f8a7652bd
Mohd Shahrukh Khan <shahrukhkhan@Mohds-MacBook-Air.local>
2024-10-16 15:29:01 +0530

    rspcs2

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,63 +1,33 @@
 class CarsController < ApplicationController
+
+
+
   before_action :set_car, only: [:show, :edit, :update, :destroy]
 
-def index
-  @cars = Car.includes(:variants)
+  # GET /cars
+  def index
+    @cars = Car.all # Adjust this if you have specific filtering logic
 
+    # Filtering by car type (new or upcoming)
+    case params[:car_type]
+    when 'new'
+      @cars = @cars.new_cars
+    when 'upcoming'
+      @cars = @cars.upcoming_cars
+    end
 
-  # Filtering by body type
-  if params[:body_type].present?
-    @cars = @cars.by_body_type(params[:body_type])
-  end
+    # Pagination using Pagy
+    pagy, paginated_cars = pagy(@cars, items: 10)
 
- 
-  # Filtering by car type (new or upcoming)
-  case params[:car_type]
-  when 'new'
-    @cars = @cars.new_cars
-  when 'upcoming'
-    @cars = @cars.upcoming_cars
-  end
-
-  # Pagination using Pagy
-  pagy, paginated_cars = pagy(@cars, items: 10)
-
-# Render JSON with cars and Pagy metadata
+    # Render JSON with cars and Pagy metadata
     render json: {
       cars: ActiveModelSerializers::SerializableResource.new(paginated_cars, each_serializer: CarSerializer),
       meta: pagination_metadata(pagy)
     }, status: :ok
-end
+  end
 
-
-
-  def create
-      catalogue = Car.new()
-      save_result = catalogue.save
-
-      if save_result
-        process_images(catalogue, params[:images])
-
-        catalogue.tags << Tag.where(id: params[:tags_id])
-
-        process_variants_images(catalogue)
-
-        render json: CatalogueSerializer.new(catalogue, serialization_options)
-                         .serializable_hash,
-               status: :created
-      else
-        render json: ErrorSerializer.new(catalogue).serializable_hash,
-               status: :unprocessable_entity
-      end
-    end
-
-  # # GET /cars/:id
-  # def show
-  #   render json: @car, serializer: CarSerializer, status: :ok
-  # end
-
-
-def show
+  # GET /cars/:id
+  def show
     # Track the viewed car in the session (limit to last 2 cars)
     session[:last_seen_cars] ||= []
     session[:last_seen_cars].delete(@car.id) # Remove the car if it's already in the list
@@ -76,7 +46,6 @@ def show
       render json: { message: 'No cars viewed recently' }, status: :ok
     end
   end
-
 
   # POST /cars
   def create
@@ -116,10 +85,134 @@ def show
   def car_params
     params.require(:car).permit(
       :name, :brand_id, :body_type, :car_types, :launch_date,
-      variants_attributes: [:id, :variant, :price, :colour, :car_id, :_destroy]
+      variants_attributes: [:id, :variant, :price, :colour, :car_id, :quantity]
     )
   end
 end
+
+
+
+
+
+# class CarsController < ApplicationController
+#   before_action :set_car, only: [:show, :edit, :update, :destroy]
+
+
+#  def index
+#     @cars = Car.all  # Adjust this if you have specific filtering logic
+#     render json: { cars: @cars }
+#   end
+
+ 
+#   # Filtering by car type (new or upcoming)
+#   case params[:car_type]
+#   when 'new'
+#     @cars = @cars.new_cars
+#   when 'upcoming'
+#     @cars = @cars.upcoming_cars
+#   end
+
+#   # Pagination using Pagy
+#   pagy, paginated_cars = pagy(@cars, items: 10)
+
+# # Render JSON with cars and Pagy metadata
+#     render json: {
+#       cars: ActiveModelSerializers::SerializableResource.new(paginated_cars, each_serializer: CarSerializer),
+#       meta: pagination_metadata(pagy)
+#     }, status: :ok
+# end
+
+
+
+#   def create
+#       catalogue = Car.new()
+#       save_result = catalogue.save
+
+#       if save_result
+#         process_images(catalogue, params[:images])
+
+#         catalogue.tags << Tag.where(id: params[:tags_id])
+
+#         process_variants_images(catalogue)
+
+#         render json: CatalogueSerializer.new(catalogue, serialization_options)
+#                          .serializable_hash,
+#                status: :created
+#       else
+#         render json: ErrorSerializer.new(catalogue).serializable_hash,
+#                status: :unprocessable_entity
+#       end
+#     end
+
+#   # # GET /cars/:id
+#   # def show
+#   #   render json: @car, serializer: CarSerializer, status: :ok
+#   # end
+
+
+# def show
+#     # Track the viewed car in the session (limit to last 2 cars)
+#     session[:last_seen_cars] ||= []
+#     session[:last_seen_cars].delete(@car.id) # Remove the car if it's already in the list
+#     session[:last_seen_cars] << @car.id # Add it to the end
+#     session[:last_seen_cars] = session[:last_seen_cars].last(2) # Keep only the last 2 cars
+
+#     render json: @car, serializer: CarSerializer, status: :ok
+#   end
+
+#   # GET /cars/last_seen
+#   def last_seen
+#     if session[:last_seen_cars].present?
+#       last_seen_cars = Car.where(id: session[:last_seen_cars])
+#       render json: last_seen_cars, each_serializer: CarSerializer, status: :ok
+#     else
+#       render json: { message: 'No cars viewed recently' }, status: :ok
+#     end
+#   end
+
+
+#   # POST /cars
+#   def create
+#     @car = Car.new(car_params)
+#     if @car.save
+#       render json: { car: @car, notice: 'Car was successfully created.' }, status: :created
+#     else
+#       render json: { errors: @car.errors.full_messages }, status: :unprocessable_entity
+#     end
+#   end
+
+#   # PATCH/PUT /cars/:id
+#   def update
+#     if @car.update(car_params)
+#       render json: { car: @car, notice: 'Car was successfully updated.' }, status: :ok
+#     else
+#       render json: { errors: @car.errors.full_messages }, status: :unprocessable_entity
+#     end
+#   end
+
+#   # DELETE /cars/:id
+#   def destroy
+#     @car.destroy
+#     render json: { notice: 'Car was successfully destroyed.' }, status: :ok
+#   end
+
+#   private
+
+#   # Find the car by ID, handle not found errors
+#   def set_car
+#     @car = Car.find(params[:id])
+#   rescue ActiveRecord::RecordNotFound
+#     render json: { error: 'Car not found' }, status: :not_found
+#   end
+
+#   # Strong parameters to allow only trusted data
+#   def car_params
+#     params.require(:car).permit(
+#       :name, :brand_id, :body_type, :car_types, :launch_date,
+#       variants_attributes: [:id, :variant, :price, :colour, :car_id, :_destroy]
+#     )
+#   end
+# end
 
 
 
--- spec/controllers/cars_controller_spec.rb
+++ spec/controllers/cars_controller_spec.rb
@@ -1,3 +1,142 @@
+# require 'rails_helper'
+
+# RSpec.describe CarsController, type: :controller do
+#   let(:brand) { create(:brand) }
+#   let!(:car) { create(:car, brand: brand) }
+#   let(:user) { create(:user) }
+#   let(:token) { JWT.encode({ user_id: user.id }, ENV['JWT_SECRET_KEY'], 'HS256') }
+
+#   before do
+#     request.headers['Token'] = "Bearer #{token}"
+#   end
+
+#   describe 'GET #index' do
+#     let!(:car1) { create(:car, brand: brand) }
+#     let!(:car2) { create(:car, brand: brand) }
+
+#     it 'returns a list of cars' do
+#       get :index
+#       expect(response).to have_http_status(:ok)
+
+#       json_response = JSON.parse(response.body)
+
+#       # Print the JSON response to check what is returned
+#       puts json_response.inspect
+
+#       expect(json_response['cars']).to be_present
+#       expect(json_response['cars'].length).to eq(3)  # Expecting all cars including the newly created one
+#       expect(json_response['cars'].map { |car| car['id'] }).to include(car.id, car1.id, car2.id)
+#     end
+#   end
+
+#   describe 'GET #show' do
+#     context 'when car exists' do
+#       it 'returns the car details' do
+#         get :show, params: { id: car.id }
+#         expect(response).to have_http_status(:ok)
+#         json_response = JSON.parse(response.body)
+#         expect(json_response['name']).to eq(car.name)
+#       end
+#     end
+
+#     context 'when car does not exist' do
+#       it 'returns a 404 not found' do
+#         get :show, params: { id: 99999 }
+#         expect(response).to have_http_status(:not_found)
+#         expect(JSON.parse(response.body)['error']).to eq('Car not found')
+#       end
+#     end
+#   end
+
+#   describe 'GET #last_seen' do
+#     context 'when there are last seen cars' do
+#       before do
+#         session[:last_seen_cars] = [car.id]
+#       end
+
+#       it 'returns the last seen cars' do
+#         get :last_seen
+#         expect(response).to have_http_status(:ok)
+#         json_response = JSON.parse(response.body)
+#         expect(json_response.first['id']).to eq(car.id)
+#       end
+#     end
+
+#     context 'when no cars have been viewed' do
+#       it 'returns a message saying no cars viewed recently' do
+#         get :last_seen
+#         expect(response).to have_http_status(:ok)
+#         expect(JSON.parse(response.body)['message']).to eq('No cars viewed recently')
+#       end
+#     end
+#   end
+
+#   describe 'POST #create' do
+#     let(:valid_params) do
+#       {
+#         car: {
+#           name: "Test Car",
+#           brand_id: brand.id,
+#           body_type: "SUV",
+#           car_types: "New Car",
+#           launch_date: "2024-10-15",
+#           variants_attributes: [
+#             { variant: 'Base Model', price: 30000, colour: 'Blue', quantity: '5' }
+#           ]
+#         }
+#       }
+#     end
+
+#     it 'creates a new car with valid attributes' do
+#       expect {
+#         post :create, params: valid_params
+#       }.to change(Car, :count).by(1)
+#       expect(response).to have_http_status(:created)
+#     end
+
+#     it 'returns an error when given invalid parameters' do
+#       post :create, params: { car: { name: nil } }
+#       expect(response).to have_http_status(:unprocessable_entity)
+#       expect(JSON.parse(response.body)['errors']).to be_present
+#     end
+#   end
+
+#   describe 'PATCH #update' do
+#     let(:update_params) do
+#       { id: car.id, car: { name: 'Updated Car Name' } }
+#     end
+
+#     it 'updates the car when given valid parameters' do
+#       patch :update, params: update_params
+#       expect(response).to have_http_status(:ok)
+#       expect(car.reload.name).to eq('Updated Car Name')
+#     end
+
+#     it 'returns an error when given invalid parameters' do
+#       patch :update, params: { id: car.id, car: { name: nil } }
+#       expect(response).to have_http_status(:unprocessable_entity)
+#       expect(JSON.parse(response.body)['errors']).to be_present
+#     end
+#   end
+
+#   describe 'DELETE #destroy' do
+#     it 'deletes the car' do
+#       expect {
+#         delete :destroy, params: { id: car.id }
+#       }.to change(Car, :count).by(-1)
+#       expect(response).to have_http_status(:ok)
+#     end
+
+#     it 'returns a 404 if the car does not exist' do
+#       delete :destroy, params: { id: 99999 }
+#       expect(response).to have_http_status(:not_found)
+#       expect(JSON.parse(response.body)['error']).to eq('Car not found')
+#     end
+#   end
+# end
+
+
+
 require 'rails_helper'
 
 RSpec.describe CarsController, type: :controller do
@@ -10,36 +149,22 @@ RSpec.describe CarsController, type: :controller do
     request.headers['Token'] = "Bearer #{token}"
   end
 
-describe 'GET #index' do
-  let!(:car1) { create(:car) }
-  let!(:car2) { create(:car) }
-  let!(:variant1) { create(:variant, car: car1, variant: 'Base Model', price: '25000', colour: 'Red') }
-  let!(:variant2) { create(:variant, car: car2, price: '30000', colour: 'Blue') }
+  describe 'GET #index' do
+    let!(:car1) { create(:car, brand: brand) }
+    let!(:car2) { create(:car, brand: brand) }
 
+    it 'returns a list of cars' do
+      get :index
+      expect(response).to have_http_status(:ok)
 
-  it 'filters by price range' do
-    get :index, params: { min_price: 15000, max_price: 25000 }
+      json_response = JSON.parse(response.body)
+      puts json_response.inspect
 
-    json_response = JSON.parse(response.body)
-
-    # Print the JSON response to check what is returned
-    puts json_response.inspect
-
-    expect(json_response['cars']).to be_present
-    expect(json_response['cars'].length).to eq(1)
-    expect(json_response['cars'].first['id']).to eq(car1.id)
+      expect(json_response['cars']).to be_present
+      expect(json_response['cars'].length).to eq(3)
+      expect(json_response['cars'].map { |car| car['id'] }).to include(car.id, car1.id, car2.id)
+    end
   end
-
-  it 'filters by body type' do
-    get :index, params: { body_type: car.body_type }
-    expect(response).to have_http_status(:ok)
-    json_response = JSON.parse(response.body)
-    expect(json_response['cars'].first['body_type']).to eq(car.body_type)
-  end
-end
-
-
-
 
   describe 'GET #show' do
     context 'when car exists' do
@@ -84,25 +209,26 @@ end
   end
 
   describe 'POST #create' do
-  let(:valid_params) do
-    {
-      car: {
-        name: "Test Car",
-        brand_id: 1, # Assuming a brand with id 1 exists
-        body_type: "SUV",
-        car_types: "New Car",
-        launch_date: "2024-10-15",
-        variants_attributes: [
-          { variant: 'Base Model', price: 30000, colour: 'Blue' }
-        ]
-      }
+let(:valid_params) do
+  {
+    car: {
+      name: "Test Car",
+      brand_id: brand.id,
+      body_type: "SUV",
+      car_types: "New Car",
+      launch_date: (Date.today + 1).to_s, # Ensure this is a future date
+      variants_attributes: [
+        { variant: 'Base Model', price: 30000, colour: 'Blue', quantity: 5 } # Ensure quantity is an integer
+      ]
     }
-  end
+  }
+end
 
     it 'creates a new car with valid attributes' do
       expect {
         post :create, params: valid_params
       }.to change(Car, :count).by(1)
+
       expect(response).to have_http_status(:created)
     end
 
@@ -110,6 +236,15 @@ end
       post :create, params: { car: { name: nil } }
       expect(response).to have_http_status(:unprocessable_entity)
       expect(JSON.parse(response.body)['errors']).to be_present
+    end
+
+    it 'logs validation errors if car creation fails' do
+      invalid_params = valid_params.deep_merge(car: { launch_date: (Date.today - 1).to_s }) # Past date
+      post :create, params: invalid_params
+      expect(response).to have_http_status(:unprocessable_entity)
+      
+      # This will help debug the error message
+      puts JSON.parse(response.body)['errors'].inspect
     end
   end
 
